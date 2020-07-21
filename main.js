c2cfdad70ab2bc04c3936d09aafd211ff6ac74f6
lyucean <lyucean@gmail.com>
2020-07-21 20:59:00 +0300

    update

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
 Vue.component('variable', {
-  props: ['item'],
+  props: ['item'] ,
   template: '#variable-template',
   watch: {
     item: {
-      handler: function () {
-        app.answer()
+      handler: function() {
+        app.answer();
       },
-      deep: true
-    }
+      deep: true,
+    },
   },
-})
+});
 
-let timeFormat = (function () {
-  function num (val) {
-    return  Math.floor(val)
+let timeFormat = (function() {
+  function num(val) {
+    return Math.floor(val);
   }
 
-  return function (time) {
+  return function(time) {
     let hours = time / 60,
-      minutes = time % 60,
-      label = ''
+        minutes = time % 60,
+        label = '';
 
-    function declOfNum (number, titles) {
-      let cases = [2, 0, 1, 1, 1, 2]
-      return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]]
+    function declOfNum(number, titles) {
+      let cases = [2, 0, 1, 1, 1, 2];
+      return titles[(number % 100 > 4 && number % 100 < 20)
+          ? 2
+          : cases[(number % 10 < 5) ? number % 10 : 5]];
     }
 
     if (0 < num(hours)) {
-      label += num(hours) + ' ' + declOfNum(num(hours), ['час', 'часа', 'часов'])
+      label += num(hours) + ' ' +
+          declOfNum(num(hours), ['час', 'часа', 'часов']);
     }
 
     if (0 < num(minutes)) {
-      label += ' ' + num(minutes) + ' ' + declOfNum(num(minutes), ['минута', 'минуты', 'минут'])
+      label += ' ' + num(minutes) + ' ' +
+          declOfNum(num(minutes), ['минута', 'минуты', 'минут']);
     }
 
-    return label
-  }
-})()
+    return label;
+  };
+})();
 
 let app = new Vue({
   el: '#app',
@@ -44,89 +48,126 @@ let app = new Vue({
     title: 'Калькулятор времени на задачу',
     dev_time_minute: 60,
     dev_time_label: '',
+    show_calc: true, // показывать ли расчёты
     estimated_time: '1 час',
     items: [
       {
-        value: 40,
-        title: 'Проектирование новой бизнес-логики',
+        value: 20,
+        time: '',
+        show: true,
+        title: 'Проектирование логики',
       },
       {
-        value: 45,
+        value: 20,
+        time: '',
+        show: true,
         title: 'Изучение текущего кода бизнес-логики',
       },
       {
-        value: 0,
+        value: 5,
+        time: '',
+        show: true,
         title: 'Изучение новых технологий и библиотек',
       },
       {
-        value: 50,
+        value: 30,
+        time: '',
+        show: true,
         title: 'Рефакторинг',
       },
       {
-        value: 25,
+        value: 50,
+        time: '',
+        show: true,
         title: 'Тестирование',
       },
       {
-        value: 30,
+        value: 10,
+        time: '',
+        show: true,
         title: 'Документирование',
       },
       {
-        value: 15,
-        title: 'Консультирование разработчиков',
+        value: 5,
+        time: '',
+        show: true,
+        title: 'Консультирование',
       },
       {
-        value: 20,
-        title: 'Ревью',
+        value: 10,
+        time: '',
+        show: true,
+        title: 'Code review',
       },
       {
         value: 0,
-        title: 'Стендап',
+        time: '',
+        show: true,
+        title: 'Конференции',
+      },
+      {
+        value: 15,
+        time: '',
+        show: true,
+        title: 'Исправление багов',
       },
       {
         value: 20,
-        title: 'Срочные багфиксы',
-      },
-      {
-        value: 20,
+        time: '',
+        show: true,
         title: 'Участие в совещаниях',
       },
       {
         value: 10,
+        time: '',
+        show: true,
         title: 'Решение инфраструктурных проблем',
       },
       {
         value: 10,
-        title: 'Деплой',
+        time: '',
+        show: true,
+        title: 'Развёртывание',
       },
       {
         value: 20,
-        title: 'Чтение Slack и ответы в личку',
-      }
-    ]
+        time: '',
+        show: true,
+        title: 'Чтение и ответы в личку',
+      },
+    ],
   },
   watch: {
-    dev_time_minute: function (val) {
-      this.dev_time_label = timeFormat(val)
-      this.answer()
+    // следим за статусом show_calc
+    show_calc: function(status) {
+      this.items.forEach(function(item) {
+        item.show = status;
+      });
+    },
+    dev_time_minute: function(val) {
+      this.dev_time_label = timeFormat(val);
+      this.answer();
     },
   },
   methods: {
-    answer: function () {
+    answer: function() {
 
-      let dev_time = this.dev_time_minute
-      let total_time = 0
+      let dev_time = this.dev_time_minute;
+      let total_time = 0;
 
-      this.items.forEach(function (item) {
-        total_time += Math.round(dev_time * item.value / 100)
-      })
+      this.items.forEach(function(item) {
+        total_time += Math.round(dev_time * item.value / 100);
 
-      total_time += parseInt(dev_time)
+        item.time = timeFormat(item.value);
+      });
 
-      this.estimated_time = timeFormat(total_time)
-    }
+      total_time += parseInt(dev_time);
+
+      this.estimated_time = timeFormat(total_time);
+    },
   },
-  created: function () {
-    this.dev_time_label = timeFormat(this.dev_time_minute)
-    this.answer()
-  }
-})
+  created: function() {
+    this.dev_time_label = timeFormat(this.dev_time_minute);
+    this.answer();
+  },
+});
